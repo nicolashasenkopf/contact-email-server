bd962856b26e078a5cf151473487c4dfd997651e
SagNurSchwitzer <sagnurschwitzer.yt@gmail.com>
2021-10-02 12:44:59 +0200

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 // Read .env file
 require('dotenv').config()
@@ -11,6 +12,7 @@ var contactRouter = require('./routes/contact');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
